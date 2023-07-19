2a3f91929692ec2f7325f6d08dd4519657b19824
kprokofi <kirill.prokofiev@intel.com>
2023-07-20 07:47:19 +0900

    added DEMA

--- otx/recipes/stages/instance-segmentation/semisl.py
+++ otx/recipes/stages/instance-segmentation/semisl.py
@@ -22,10 +22,10 @@ custom_hooks = [
     dict(
         type="UnbiasedTeacherHook",
         epoch_momentum=0.0,
-        start_epoch=16
+        start_epoch=8
     )
 ]
 
-adaptive_ema = dict(epoch_momentum=0.4)
+adaptive_ema = dict(epoch_momentum=0.1)
 ignore = True
 find_unused_parameters = True
