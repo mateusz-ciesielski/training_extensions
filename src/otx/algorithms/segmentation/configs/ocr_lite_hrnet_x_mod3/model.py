518de93fb87ae62b15da9b300289cc2f7903dfd8
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-09-08 09:00:25 +0900

    Update configs

--- src/otx/algorithms/segmentation/configs/ocr_lite_hrnet_x_mod3/model.py
+++ src/otx/algorithms/segmentation/configs/ocr_lite_hrnet_x_mod3/model.py
@@ -52,4 +52,4 @@ model = dict(
 load_from = "https://storage.openvinotoolkit.org/repositories/openvino_training_extensions\
 /models/custom_semantic_segmentation/litehrnetxv3_imagenet1k_rsc.pth"
 
-fp16 = dict(loss_scale=512.0)
+#fp16 = dict(loss_scale=512.0)
