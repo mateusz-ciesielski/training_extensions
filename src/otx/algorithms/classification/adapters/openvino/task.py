2f89374831dd89f7a607366278782e72c68ba2bb
eunwoosh <eunwoo.shin@intel.com>
2023-09-19 13:58:11 +0900

    eval task save avg_time_per_images as result

--- src/otx/algorithms/classification/adapters/openvino/task.py
+++ src/otx/algorithms/classification/adapters/openvino/task.py
@@ -160,8 +160,13 @@ class ClassificationOpenVINOTask(IDeploymentTask, IInferenceTask, IEvaluationTas
         self.hparams = self.task_environment.get_hyper_parameters(ClassificationConfig)
         self.model = self.task_environment.model
         self.inferencer = self.load_inferencer()
+        self._avg_time_per_image = None
         template_file_path = self.task_environment.model_template.model_template_path
         self._base_dir = os.path.abspath(os.path.dirname(template_file_path))
+
+    @property
+    def avg_time_per_image(self):
+        return self._avg_time_per_image
 
     def load_inferencer(self) -> ClassificationOpenVINOInferencer:
         """load_inferencer function of ClassificationOpenVINOTask."""
@@ -250,7 +255,8 @@ class ClassificationOpenVINOTask(IDeploymentTask, IInferenceTask, IEvaluationTas
 
         self.inferencer.await_all()
 
-        logger.info(f"Avg time per image: {total_time/len(dataset)} secs")
+        self._avg_time_per_image = total_time / len(dataset)
+        logger.info(f"Avg time per image: {self._avg_time_per_image} secs")
         logger.info(f"Total time: {total_time} secs")
         logger.info("Classification OpenVINO inference completed")
 
