67bb05e043c07e158033f3488edc8066d758fc5c
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2023-10-25 10:30:01 +0900

    Enable FP 16

--- src/otx/algorithms/segmentation/configs/ocr_lite_hrnet_s_mod2/model.py
+++ src/otx/algorithms/segmentation/configs/ocr_lite_hrnet_s_mod2/model.py
@@ -51,4 +51,4 @@ model = dict(
 load_from = "https://storage.openvinotoolkit.org/repositories/openvino_training_extensions\
 /models/custom_semantic_segmentation/litehrnetsv2_imagenet1k_rsc.pth"
 
-#fp16 = dict(loss_scale=512.0)
+fp16 = dict(loss_scale=512.0)
